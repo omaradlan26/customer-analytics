9ccf5603cb48297333698b8853e63e7c6779534d
omaradlan26 <omaradlan2605@gmail.com>
2026-04-10 20:27:55 +0200

    Fix print statements formatting in visualize.py

--- visualize.py
+++ visualize.py
@@ -2,18 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-print("🚀 Visualization started")
+print(" Visualization started")
 
 df = pd.read_csv("data_preprocessed.csv")
 
-# 1. Histogram
 df.hist(figsize=(10,8))
 
-# 2. Correlation heatmap
 plt.figure()
 sns.heatmap(df.corr(), annot=False)
 
-# Save all plots
 plt.savefig("summary_plot.png")
 
-print("✅ summary_plot.png created")
+print(" summary_plot.png created")
